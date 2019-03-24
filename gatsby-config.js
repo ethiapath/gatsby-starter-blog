b7b9cce9ddaf7cfe6bd4f3ac27f5a1e6d46604ed
Hargobind Atwal <ethaipath@gmail.com>
2019-03-24 15:37:08 -0700

    Customizing data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    author: `Hargobind Atwal`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `icftmachine`,
     },
   },
   plugins: [
@@ -57,7 +57,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
+        name: `Code`,
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
